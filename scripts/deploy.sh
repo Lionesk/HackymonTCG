fdeaca91494fc4e8a8991da49b4ae68da022d074
Emilio Assuncao <emilio.assuncao@gmail.com>
2018-05-26 00:48:50 -0400

    Piping ssh commands to remote shell using a HEREDOC -- was able to test deploy script from machine

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,15 @@
 #!/bin bash
 scp package.tar.gz dev@dumblev4.encs.concordia.ca:
 
-ssh dev@dumblev4.encs.concordia.ca
-cd /var/www/HackymonTCG
-rm -rf *
-tar xzf ~/package.tar.gz
-chown -R dev: .
+ssh dev@dumblev4.encs.concordia.ca <<EOF
+    cd /var/www/HackymonTCG
+    rm -rf *
+    tar xzf ~/package.tar.gz
+    chown -R dev: .
 
-cd /var/www/HackymonTCG/bundle/programs/server
-npm install --production
+    cd /var/www/HackymonTCG/bundle/programs/server
+    npm install --production
 
-rm ~/package.tar.gz
-logout
-
+    rm ~/package.tar.gz
+EOF
 curl http://dumblev4.encs.concordia.ca
