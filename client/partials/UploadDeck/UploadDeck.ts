c4cb01d75297af64f632e022357c672a29f5451a
Emilio Assuncao <emilio.assuncao@gmail.com>
2018-06-07 14:26:09 -0400

    Revert "Added base Cards schema to mongo"

--- client/partials/UploadDeck/UploadDeck.ts
+++ client/partials/UploadDeck/UploadDeck.ts
@@ -1,15 +1,8 @@
 import { Template } from 'meteor/templating'
-import { Cards, CardType, TrainerCat } from "../../../collections";
 
 Template.UploadDeck.events({
     'click .printToServer':function(){
-        Cards.insert({
-            type: CardType.TRAINER,
-            category: TrainerCat.STADIUM,
-            name: "Cool City Stadium",
-        });
-        console.log(Cards.find().fetch());
-        Meteor.call('printToServerConsole'); //defined in lib/utilities.ts
+        Meteor.call('printToServerConsole');//defined in lib/utilities.ts
     },
 });
 
--- lib/utilities.ts
+++ lib/utilities.ts
@@ -1,14 +1,8 @@
 import { Meteor } from "meteor/meteor";
-import { Cards, CardType, PokemonCat } from "../collections";
 
 Meteor.methods({
-    printToServerConsole: () => {
+    printToServerConsole: function () {
         if(Meteor.isServer){
-            // Cards.insert({
-            //     type: CardType.POKEMON,
-            //     category: PokemonCat.BASIC,
-            // });
-            console.log(Cards.find().count());
             console.log("Printing To Server Console");
         }
     }
