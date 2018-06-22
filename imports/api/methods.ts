0fe802f9e95508383f1bf430248cb66a87747443
JohnyMastro <johnymastro@gmail.com>
2018-06-22 11:32:51 -0400

    Added global gameManager on server and the ability to pass the gameState to the front-end

--- imports/api/methods.ts
+++ imports/api/methods.ts
@@ -1,10 +1,16 @@
 import { Meteor } from "meteor/meteor";
 import { Cards, CardType, PokemonCat } from "./collections";
-
+import {gameManager} from '../startup/server/startup'
 Meteor.methods({
     printToServerConsole: function () {
         if(Meteor.isServer){
             console.log("Printing To Server Console");
+        }
+    },
+    getInitialGameState: function () {
+        if(Meteor.isServer){
+            console.log("gameManager.gameState: " + gameManager.gameState);
+            return gameManager.gameState;
         }
     }
 });
