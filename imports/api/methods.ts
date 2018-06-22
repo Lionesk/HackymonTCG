961180bed035a38ce9e22604d96dc54a6fcf16b7
Jesse Tremblay <butt-head_3000@hotmail.com>
2018-06-21 23:16:28 -0400

    Added uploader element

--- imports/api/methods.ts
+++ imports/api/methods.ts
@@ -2,9 +2,24 @@ import { Meteor } from "meteor/meteor";
 import { Cards, CardType, PokemonCat } from "./collections";
 
 Meteor.methods({
-    printToServerConsole: function () {
+    printToServerConsole() {
         if(Meteor.isServer){
             console.log("Printing To Server Console");
         }
-    }
+    },
+    uploadCards(data: { cardString: string }) {
+        if (Meteor.isServer) {
+            console.log(data.cardString);
+        }
+    },
+    uploadAbilities(data: { cardString: string }) {
+        if (Meteor.isServer) {
+            console.log(data.cardString);
+        }
+    },
+    uploadDeck(data: { cardString: string }) {
+        if (Meteor.isServer) {
+            console.log(data.cardString);
+        }
+    },
 });
--- imports/ui/partials/UploadDeck/UploadDeck.ts
+++ imports/ui/partials/UploadDeck/UploadDeck.ts
@@ -2,18 +2,69 @@ import { Template } from 'meteor/templating'
 import './UploadDeck.html'
 import { Cards, CardType, TrainerCat } from "../../../api/collections";
 
+enum UploadTypes {
+    CARDS = "cards",
+    DECK = "deck",
+    ABIL = "abilities"
+}
+
+const UploadMap: { [key in UploadTypes]: (string) => void } = {
+    cards: uploadCards,
+    deck: uploadDeck,
+    abilities: uploadAbilities,
+}
+
 Template.UploadDeck.events({
-    'click .printToServer':function(){
-        Cards.insert({
-            type: CardType.TRAINER,
-            category: TrainerCat.STADIUM,
-            name: "Cool City Stadium",
-        });
-        
-        Meteor.call('printToServerConsole'); //defined in lib/utilities.ts
-        console.log(Cards.find().count());
-    
+    'dragover .uploadDropper'(e, t) {
+        e.preventDefault();
+      },
+      'dragenter .uploadDropper'(e, t) {
+        e.preventDefault();
+        // change style of drop zone
+      },
+    async 'drop .uploadDropper'(event: any, target: any) {
+        event.preventDefault();
+        const file: File = event.originalEvent.dataTransfer.files[0];
+        if (file) {
+            const fileString: string = await loadFile(file);
+            console.log(fileString);
+            UploadMap[this.uploadType](fileString); // use target instead of this for better typing
+        } else {
+            throw "invalid file";
+        }
     },
+    async 'change #file'(event: any, target: any) {
+        const file: File = event.originalEvent.target.files[0]; // only handle one file for now
+        if (file) {
+            const fileString: string = await loadFile(file);
+            console.log(fileString);
+            UploadMap[this.uploadType](fileString); // use target instead of this for better typing
+        } else {
+            throw "invalid file";
+        }
+    }
 });
 
+async function loadFile(file: File): Promise<string> {
+    let f: FileReader = new FileReader();
+    let data: string;
+    return new Promise<string>((resolve, reject) => {
+        f.onloadend = (ev: any) => {
+            resolve(ev.originalTarget.result);
+            return ev;
+        };
+        f.readAsText(file);
+    });
+}
 
+function uploadCards(data: string): void {
+    Meteor.call("uploadCards", { cardString: data });
+}
+
+function uploadAbilities(data: string) {
+    Meteor.call("uploadAbilities", { cardString: data });
+}
+
+function uploadDeck(data: string): void {
+    Meteor.call("uploadDeck", { cardString: data });
+}
