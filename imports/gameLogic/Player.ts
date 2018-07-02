1a0d3385a7470ebe6467a01ee321ef9a1ad6b085
JohnyMastro <johnymastro@gmail.com>
2018-07-02 16:02:54 -0400

    moved options displaying more info

--- imports/gameLogic/Player.ts
+++ imports/gameLogic/Player.ts
@@ -7,12 +7,14 @@ export class Player{
     bench:PlayableCard[];
     prize:PlayableCard[];
     deck:PlayableCard[];
+    discard:PlayableCard[];
 
     constructor(player?:Player){
        this.hand = new Array<PlayableCard>();
        this.deck = null;
        this.bench = new Array<PlayableCard>();
        this.prize = new Array<PlayableCard>();
+       this.discard = new Array<PlayableCard>(0);
     }
 
 }
