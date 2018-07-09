fd10e8f2a27c29c4ba939a7f455d262e39d0912f
JohnyMastro <johnymastro@gmail.com>
2018-07-09 17:07:38 -0400

    added restrictions for second round, and fixed AI from playing evolution cards straight to bench

--- imports/gameLogic/GameState.ts
+++ imports/gameLogic/GameState.ts
@@ -4,28 +4,27 @@ export interface GameStateInterface{
     userid:string;
     player:Player;
     ai:Player;
-    round:number;
     humanFirst:boolean;
     energyPlayed:boolean;
     isFirstRound:boolean;
+    isSecondRound:boolean;
 }
 
 export class GameState implements GameStateInterface{
     userid:string;
     player:Player;
     ai:Player;
-    round:number;
     humanFirst:boolean;
     energyPlayed:boolean;
     isFirstRound:boolean;
-    
+    isSecondRound:boolean;
 
     constructor(userid:string){
         this.player = new Player();
         this.ai = new Player();
-        this.round = 0;
         this.userid=userid;
         this.energyPlayed=false;
         this.isFirstRound=true;
+        this.isSecondRound=true;
     }
 }
