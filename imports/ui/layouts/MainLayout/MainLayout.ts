2f89d1c2ae1b8e9314deceae5249fdd0df7415b0
Emilio Assuncao <emilio.assuncao@gmail.com>
2018-07-11 14:52:44 -0400

    Put LoginButtons in simple NavBar accross the top, and made Header only display on the LandingLayout and not the GameLayout

--- imports/ui/layouts/MainLayout/MainLayout.ts
+++ imports/ui/layouts/MainLayout/MainLayout.ts
@@ -1,4 +1,4 @@
 import { Template } from 'meteor/templating'
 import "./MainLayout.html"
 import "./MainLayout.css"
-import "../../partials/Header/Header.ts"
+import "../../partials/navbar/NavBar.ts"
