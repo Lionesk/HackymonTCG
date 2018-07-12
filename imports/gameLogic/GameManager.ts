c43b5500170d0e1114b347d99e910670c063e467
nabokinm <nabokinm@gmail.com>
2018-07-12 19:35:37 -0400

    fix conflicts after typescript rule change

--- imports/gameLogic/GameManager.ts
+++ imports/gameLogic/GameManager.ts
@@ -488,7 +488,8 @@ export module GameManager {
         
         for(let i=0; i<7;i++){
             let card =player.hand.pop();
-            
+           
+            if(card !== undefined)
             player.deck.push(card);
 
         }
