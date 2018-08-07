63642419e42b8750cfdbaf2c97fb47d2b6f54afe
JohnyMastro <johnymastro@gmail.com>
2018-08-06 22:14:12 -0400

    ability to string function done

--- imports/ui/gameComponents/Card/Ability/Ability.ts
+++ imports/ui/gameComponents/Card/Ability/Ability.ts
@@ -12,16 +12,53 @@ Template.Action.helpers({
         return action.type === "applystat";        
     },
     topActionString:function(action: AbilityAction){
-        let str = "Apply ";
+        let str = "";
+        if(action.choice){
+            str = str.concat("Based on "+action.choice+" choice, apply ");
+        }else{
+            str = str.concat("Apply ");
+        }
+        if(action.source){
+            str = str.concat("from "+action.source+", ");
+        }
         if(action.amount){
             str = str.concat(action.amount+" ");
         }
         if(action.type){
             str = str.concat(action.type+" ");
         }
-        if(action.type){
-            str = str.concat(action.type+" ");
+        if(action.status){
+            str = str.concat("apply "+action.status+" ");
         }
+        if(action.target){
+            str = str.concat("to "+action.target+" ");
+        }        
+        if(action.amountFunction){
+            str = str.concat("by "+action.amountOperator+" by "+ action.amountFunction);
+        }
+        if(action.amountFunctionTarget){
+            str = str.concat("of "+action.amountFunctionTarget+" ");
+        }
+        return str;
+    },
+    bottomActionString:function(action: AbilityAction){
+        let str = "";
+        if(!action){
+            return str;
+        }
+        if(!action.filter){
+            return str;
+        }
+        if(action.filter.category){
+            str = str.concat("Filter by "+action.filter.category+" ");
+        }
+        if(action.filter.top){
+            str = str.concat("on "+action.filter.top?"top":"" +" ");
+        }
+        if(action.filter.top){
+            str = str.concat("by "+action.filter.count +" cards ");
+        }
+        return str;
     }
 });
 Template.Ability.helpers({
